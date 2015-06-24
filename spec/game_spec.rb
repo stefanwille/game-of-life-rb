b7b14a665e1a71af92ff5c3a8cfca58bd3390f3e
Stefan Wille <github@stefanwille.com>
2015-06-24 22:33:22 +0200

    .

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,36 @@
 require 'rspec'
+require 'grid'
+
+describe 'Game' do
+  describe '#live?' do
+    it 'returns if a cell should be live in the next grid' do
+      game = Game.new(3,3)
+      expect(game.live?(true, 0)).to be false
+      expect(game.live?(true, 1)).to be false
+      expect(game.live?(true, 2)).to be true
+      expect(game.live?(true, 3)).to be true
+      expect(game.live?(true, 4)).to be false
+      expect(game.live?(true, 5)).to be false
+
+      expect(game.live?(false, 0)).to be false
+      expect(game.live?(false, 1)).to be false
+      expect(game.live?(false, 2)).to be false
+      expect(game.live?(false, 3)).to be true
+      expect(game.live?(false, 4)).to be false
+      expect(game.live?(false, 5)).to be false
+    end
+  end
+
+  describe '#to_s' do
+    it 'convert the current grid to a string, suitable for printing to the terminal' do
+      game = Game.new(3,3)
+      game.set(0, 0, true)
+      game.set(1, 0, true)
+      game.set(2, 0, true)
+      expect(game.to_s).to eq("XXX\n   \n   \n")
+    end
+  end
+end
 
 describe 'Grid' do
 	describe '#live' do
@@ -17,51 +49,27 @@ describe 'Grid' do
       expect(grid.neighbours(0, 0)).to eq(2)
       expect(grid.neighbours(0, 1)).to eq(1)
     end
-	end
-end
-
-require 'matrix'
-
-class Matrix
-  def []=(row, column, value)
-    @rows[row][column] = value
   end
 end
 
-class Grid
-	def initialize(width, height)
-		@width = width
-		@height = height
-		@cells = Matrix.build(width, height) { false }
-	end
-
-	def set(x, y, live)
-		@cells[x, y] = live
-	end
-
-	def cell(x, y)
-		@cells[x, y]
-	end
-
-  def neighbours(x, y)
-    [
-      cell_count(x-1, y-1),
-      cell_count(x, y-1),
-      cell_count(x+1, y-1),
-      cell_count(x-1, y),
-      cell_count(x+1, y),
-      cell_count(x-1, y+1),
-      cell_count(x, y+1),
-      cell_count(x+1, y+1)
-    ].inject(:+)
+class Game
+  def initialize(width, height)
+    @grid = Grid.new(width, height)
   end
 
-  private def cell_count(x, y)
-    return 0 if out_of_bounds?(x, y)
-    cell(x, y) ? 1 : 0
+  def set(x, y, live)
+    @grid.set(x, y, live)
   end
 
-  private def out_of_bounds?(x, y)
-    x < 0 || x >= @width || y < 0 || y >= @height
+  def live?(live, neighbours)
+    if live
+      neighbours >=2 && neighbours <= 3
+    else
+      neighbours == 3
+    end
+  end
+
+  def to_s
+    @grid.to_s
   end
 end
